0c81c51daff4b4d1b3b7796a795d067e9d313512
hank amadeus <hankamadeus@email.com>
2020-05-04 20:14:09 -0400

    created search input for city, disabled prettier extension and need to restart vs code

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Input bar with search button that allows user to search for city
+// User city input then stored to empty variable
+// Must store and grab user input to/from localstorage
+
+// Going to need to create three AJAX calls
+// One will show the city name, date, current temp(F), current humidity, current wind speed
+// One will to get UV index
+// The other will display the 5-day forcast for the user's searched city and display, date, weather image, temp(f), and the humdity
+
+// Will moment.js be needed for current date??? or is it already in api?
