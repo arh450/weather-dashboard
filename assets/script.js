25e7ddda063a494153a7eafbea8088151497f3b7
hank amadeus <hankamadeus@email.com>
2020-05-06 12:48:27 -0400

    established api key for openweather and hard queried example city

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,50 @@
 // The other will display the 5-day forcast for the user's searched city and display, date, weather image, temp(f), and the humdity
 
 // Will moment.js be needed for current date??? or is it already in api?
+
+$(document).ready(function () {
+
+
+    // 1. Get Open Weather API Key
+    var owApiKey = "fcb202793a3b50951b0129bcb32cb07d";
+    // 2. Create Query URL for Current Weather
+    var currentURL = `http://api.openweathermap.org/data/2.5/weather?q=springfield&appid=${owApiKey}&units=imperial`;
+
+    // console.log(currentURL);
+
+    // 3. Make AJAX call for current weather of Boston, MA (example in TEMP F)
+
+    $.get(currentURL).then(function (response) {
+        console.log(currentURL);
+        console.log(response);
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
